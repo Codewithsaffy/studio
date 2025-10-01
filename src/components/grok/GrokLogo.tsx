173881b4032df3d75613ff084633f1f57412ae63
Muhammad Yousuf <codewithsaffy@gmail.com>
2025-10-01 18:06:11 +0000

    remove it from bg

--- src/components/grok/GrokLogo.tsx
+++ src/components/grok/GrokLogo.tsx
@@ -1,33 +1,35 @@
 import { cn } from "@/lib/utils";
 
-export function GrokLogo({ className }: { className?: string }) {
+export function GrokLogo({ className, ...props }: { className?: string }) {
   return (
-    <svg
-      width="40"
-      height="40"
-      viewBox="0 0 24 24"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      className={cn("text-foreground", className)}
-    >
-      <path
-        d="M12 2.5L13.84 8.16L19.5 10L13.84 11.84L12 17.5L10.16 11.84L4.5 10L10.16 8.16L12 2.5Z"
-        stroke="currentColor"
-        strokeWidth="1.5"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M4.5 18.5L5.42 16.34L7.5 15.5L5.42 14.66L4.5 12.5L3.58 14.66L1.5 15.5L3.58 16.34L4.5 18.5Z"
-        stroke="currentColor"
-        strokeWidth="1.5"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M19.5 18.5L18.58 16.34L16.5 15.5L18.58 14.66L19.5 12.5L20.42 14.66L22.5 15.5L20.42 16.34L19.5 18.5Z"
-        stroke="currentColor"
-        strokeWidth="1.5"
-        strokeLinejoin="round"
-      />
-    </svg>
+    <div className={cn("flex items-center justify-center", className)} {...props}>
+      <svg
+        width="1em"
+        height="1em"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        className="text-foreground"
+      >
+        <path
+          d="M12 2.5L13.84 8.16L19.5 10L13.84 11.84L12 17.5L10.16 11.84L4.5 10L10.16 8.16L12 2.5Z"
+          stroke="currentColor"
+          strokeWidth="1.5"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M4.5 18.5L5.42 16.34L7.5 15.5L5.42 14.66L4.5 12.5L3.58 14.66L1.5 15.5L3.58 16.34L4.5 18.5Z"
+          stroke="currentColor"
+          strokeWidth="1.5"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M19.5 18.5L18.58 16.34L16.5 15.5L18.58 14.66L19.5 12.5L20.42 14.66L22.5 15.5L20.42 16.34L19.5 18.5Z"
+          stroke="currentColor"
+          strokeWidth="1.5"
+          strokeLinejoin="round"
+        />
+      </svg>
+    </div>
   );
 }
