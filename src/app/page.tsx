1d45f90a6fff57ff3d2651e62165f5565def6082
AbdulSamad94 <abdulsamadsiddiqui2000@gmail.com>
2025-10-12 22:14:51 +0500

    bug fixing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
-'use client';
+"use client";
 
-import { ChatView } from '@/components/grok/ChatView';
-import { ChatProvider } from '@/context/ChatContext';
-import { SidebarProvider, SidebarInset } from '@/components/ui/sidebar';
-import { Header } from '@/components/grok/Header';
-import { type CSSProperties } from 'react';
+import { ChatView } from "@/components/grok/ChatView";
+import { ChatProvider } from "@/context/ChatContext";
+import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
+import { Header } from "@/components/grok/Header";
+import { type CSSProperties } from "react";
 
 export default function Home() {
   return (
     <SidebarProvider
       style={
         {
-          '--sidebar-width': '280px',
-          '--sidebar-width-icon': '80px',
+          "--sidebar-width": "280px",
+          "--sidebar-width-icon": "80px",
         } as CSSProperties
       }
     >
