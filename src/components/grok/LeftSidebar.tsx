6ed38c218ed92e79718bd72df110a7edd6210624
Muhammad Yousuf <codewithsaffy@gmail.com>
2025-10-14 13:15:43 +0000

    make the size small (_for element <Building>_)

--- src/components/grok/LeftSidebar.tsx
+++ src/components/grok/LeftSidebar.tsx
@@ -184,7 +184,7 @@ export function LeftSidebar() {
                   >
                     <div className="flex items-center gap-3">
                        <Link href="/vendors" className="flex items-center gap-3">
-                        <Building />
+                        <Building className="h-4 w-4" />
                         <span className="group-data-[collapsible=icon]:hidden">
                           Vendors
                         </span>
