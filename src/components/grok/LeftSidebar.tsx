02546fe35462e88c94bb19e49be3be4fd09afbe0
Muhammad Yousuf <codewithsaffy@gmail.com>
2025-10-14 13:03:52 +0000

    channge this icon to icon Building  (_for element <Store>_)

--- src/components/grok/LeftSidebar.tsx
+++ src/components/grok/LeftSidebar.tsx
@@ -184,7 +184,7 @@ export function LeftSidebar() {
                   >
                     <div className="flex items-center gap-3">
                        <Link href="/vendors" className="flex items-center gap-3">
-                        <Store />
+                        <Building />
                         <span className="group-data-[collapsible=icon]:hidden">
                           Vendors
                         </span>
